53ec689d8d64288521594e882680a2aeef560140
Prabhakar Singh <pksingh15007@gmail.com>
2021-03-16 08:40:56 +0530

    Delete JavaExamples directory

--- JavaExamples/FirstJavaProject/src/com/day2/Example1.java
+++
@@ -1,9 +0,0 @@
-package com.day2;
-
-public class Example1 {
-
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World");
-	}
-}
--- JavaExamples/FirstJavaProject/src/com/day3/Example8.java
+++
@@ -1,9 +0,0 @@
-package com.day3;
-
-public class Example8 {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
